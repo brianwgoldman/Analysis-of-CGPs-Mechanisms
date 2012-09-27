8b1e5f695712c66e985fb2ac1eb8f1ead5f19bdb
Goldman <brianwgoldman@acm.org>
2012-09-27 11:51:42 -0400

    Fix to hpcparse

--- hpcparse.py
+++ hpcparse.py
@@ -1,12 +1,14 @@
 import json
 import sys
+from os import path
 from collections import defaultdict
 from main import combine_results
 
 
 groupings = defaultdict(list)
 for filename in sys.argv[1:]:
-    problem, nodes, rate, version, _ = filename.split('_')
+    base = path.basename(filename)
+    problem, nodes, rate, version, _ = base.split('_')
     with open(filename, 'r') as f:
         data = json.load(f)
     groupings[problem, nodes, rate, version].append(data[1])
