3f7edafd8aed06cb72f036dba6356716a3eebd6c
Goldman <brianwgoldman@acm.org>
2012-10-11 09:22:45 -0400

    Minor tweeks to hpcparse, updated once.cfg to use 10million evals

--- hpcparse.py
+++ hpcparse.py
@@ -8,18 +8,27 @@ from main import combine_results
 groupings = defaultdict(list)
 for filename in sys.argv[1:]:
     base = path.basename(filename)
-    problem, nodes, rate, version, _ = base.split('_')
-    with open(filename, 'r') as f:
-        data = json.load(f)
-    groupings[problem, int(nodes), float(rate), version].append(data[1])
+    try:
+        problem, nodes, rate, version, _ = base.split('_')
+        with open(filename, 'r') as f:
+            data = json.load(f)
+        groupings[problem, int(nodes), float(rate), version].append(data[1])
+    except ValueError:
+        print filename, 'FAILED'
 
 bests = defaultdict(tuple)
-for key, results in groupings.iteritems():
+for key, results in sorted(groupings.iteritems()):
     problem, nodes, rate, version = key
     combined = combine_results(results)
-    fitness = combined['fitness']
+    fitness = combined['fitness'][0], -combined['evals'][0], combined['phenotype'][0]
+    print key, fitness
     bests[problem, version] = max(bests[problem, version],
                                   (fitness, nodes, rate))
+    bests[problem, nodes, rate] = max(bests[problem, nodes, rate],
+                                  (fitness, version))
 
+print
+print 'BEST'
+print
 for best in sorted(bests.items()):
     print best
