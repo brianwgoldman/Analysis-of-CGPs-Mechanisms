67aacd9640c5ee82f55f9981a3a40edfe1a7c739
Goldman <brianwgoldman@acm.org>
2012-10-12 14:52:46 -0400

    Updated submission machinery

--- alot.sh
+++ alot.sh
@@ -1,10 +1,18 @@
-MUT=$1
-START=$2
-END=$3
+START=$1
+END=$2
 
 for SEED in `seq $START $END`
 do
-	./runone.sh quartic 100 $MUT $SEED
-	./runone.sh parity 3000 $MUT $SEED
-	./runone.sh multiply 3000 $MUT $SEED
+	./runone.sh quartic 100 0.1 $SEED onemut.sub
+	./runone.sh parity 3000 0.1 $SEED onemut.sub
+	./runone.sh multiply 3000 0.1 $SEED onemut.sub
+	for MUT in 0.001 0.002 0.004 0.008 0.01 0.02 0.04 0.08 0.1
+	do
+		for SUB in mutate.sub reeval.sub
+		do
+			./runone.sh quartic 100 $MUT $SEED $SUB
+			./runone.sh parity 3000 $MUT $SEED $SUB
+			./runone.sh multiply 3000 $MUT $SEED $SUB
+		done
+	done
 done
--- runone.sh
+++ runone.sh
@@ -2,10 +2,8 @@ PROBLEM=$1
 NODES=$2
 MUT=$3
 SEED=$4
+SUB=$5
 
-for i in mutate.sub reeval.sub
-do
-	sed -e "s/PROBLEM/$PROBLEM/g" -e "s/NODES/$NODES/g" -e "s/MUT/$MUT/g" -e "s/SEED/$SEED/g" $i > temp.sub
-	qsub temp.sub
-	rm temp.sub
-done
+sed -e "s/PROBLEM/$PROBLEM/g" -e "s/NODES/$NODES/g" -e "s/MUT/$MUT/g" -e "s/SEED/$SEED/g" $SUB > temp.sub
+qsub temp.sub
+rm temp.sub
