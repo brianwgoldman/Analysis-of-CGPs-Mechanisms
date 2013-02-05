3c4490dac2690ad604587169965bd2f9ee4f8762
Goldman <brianwgoldman@acm.org>
2013-02-05 15:50:47 -0500

    Update to queue handling

--- runone.sh
+++ runone.sh
@@ -3,8 +3,10 @@ DUPLICATE=$2
 ORDERING=$3
 NODES=$4
 MUT=$5
-SUB=array.sub
+SEED=$6
+SUB=sub.sub
 
-sed -e "s/PROBLEM/$PROBLEM/g" -e "s/NODES/$NODES/g" -e "s/MUT/$MUT/g" -e "s/DUPLICATE/$DUPLICATE/g" -e "s/ORDERING/$ORDERING/g" $SUB > temp.sub
-qsub -t 1-4 temp.sub
+sed -e "s/PROBLEM/$PROBLEM/g" -e "s/NODES/$NODES/g" -e "s/MUT/$MUT/g" -e "s/DUPLICATE/$DUPLICATE/g" -e "s/ORDERING/$ORDERING/g"\
+    -e "s/SEED/$SEED/g" $SUB > temp.sub
+qsub temp.sub
 rm temp.sub
