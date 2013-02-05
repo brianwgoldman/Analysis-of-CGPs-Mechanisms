7659fb53df925b481031816777797fb89019e262
Goldman <brianwgoldman@acm.org>
2013-02-05 08:49:13 -0500

    Updates to queue handling scripts

--- alot.sh
+++ alot.sh
@@ -1,20 +1,17 @@
-START=$1
-END=$2
-
-for SEED in `seq $START $END`
+for problem in encode decode multiply parity
 do
-	./runone.sh parity 3000 0.1 $SEED onemut.sub
-	./runone.sh multiply 3000 0.1 $SEED onemut.sub
-	./runone.sh mux 3000 0.1 $SEED onemut.sub
-	./runone.sh demux 3000 0.1 $SEED onemut.sub
-	for MUT in 0.0001 0.0002 0.0004 0.0008 0.001 0.002 0.004 0.008 0.01 0.02 0.04 0.08 0.1 0.2 0.4 0.8
+	for ordering in normal reorder dag
 	do
-		for SUB in mutate.sub reeval.sub
+		for nodes in 50 100 200 500 1000 2000 5000 10000
 		do
-			./runone.sh parity 3000 $MUT $SEED $SUB
-			./runone.sh multiply 3000 $MUT $SEED $SUB
-			./runone.sh mux 3000 $MUT $SEED $SUB
-			./runone.sh demux 3000 $MUT $SEED $SUB
+			echo ./runone.sh $problem single $ordering $nodes 1
+			for duplicate in skip accumulate
+			do
+				for mut in 0.05 0.02 0.01 0.005 0.002 0.001 0.0005 0.0002
+				do
+					echo ./runone.sh $problem $duplicate $ordering $nodes $mut
+				done
+			done
 		done
 	done
 done
--- runone.sh
+++ runone.sh
@@ -1,9 +1,10 @@
 PROBLEM=$1
-NODES=$2
-MUT=$3
-SEED=$4
-SUB=$5
+DUPLICATE=$2
+ORDERING=$3
+NODES=$4
+MUT=$5
+SUB=array.sub
 
-sed -e "s/PROBLEM/$PROBLEM/g" -e "s/NODES/$NODES/g" -e "s/MUT/$MUT/g" -e "s/SEED/$SEED/g" $SUB > temp.sub
-qsub temp.sub
+sed -e "s/PROBLEM/$PROBLEM/g" -e "s/NODES/$NODES/g" -e "s/MUT/$MUT/g" -e "s/DUPLICATE/$DUPLICATE/g" -e "s/ORDERING/$ORDERING/g" $SUB > temp.sub
+qsub -t 1-4 temp.sub
 rm temp.sub
